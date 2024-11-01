b784e30c68713a866ae0cd89eb97ca3e7ef99e46
Vitaliy Hnatenko <vitaliy.gnatenko@apriorit.com>
2024-11-01 13:54:56 +0200

    Initial Commit

---
+++ NetworkExtension/NetworkExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkExtensionApp.swift
+//  NetworkExtension
+//
+//  Created by user on 01.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
